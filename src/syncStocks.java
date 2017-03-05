7d64f58988756da82f1e998fca62dfbf5f068b1a
Jacob <jacob.millner@outlook.com>
2017-03-04 22:12:53 -0500

    stock data repo

--- src/syncStocks.java
+++ src/syncStocks.java
@@ -3,27 +3,31 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class syncStocks {
-	public static void main(String args[]) {
+	
+	public static void start(String args[]) {
 
 		File file = new File("stockStuff.db");
 		Connection c = null;
+		
 		// check if the db exists before we try to sync
 		if (file.exists()) {
 			try {
+				
 				Class.forName("org.sqlite.JDBC");
 				c = DriverManager.getConnection("jdbc:sqlite:stockStuff.db");
+				
 			} catch (Exception e) {
 				System.err.println(e.getClass().getName() + ": " + e.getMessage());
 				System.exit(0);
 			}
-			System.out.println("Opened database successfully, starting sync.");
+			
+			System.out.println("Opened git puldatabase successfully, starting sync.");
 
 			syncData(c); // sync that data!
 
 		} else {
 			System.out.print("The db doesn't exist. Run the database setup script first.");
 		}
-
 	}
 
 	private static void syncData(Connection c) {
@@ -34,7 +38,7 @@ public class syncStocks {
 
 	}
 	
-	private static void gitPull() {
+	private static void pullStockData() {
 
 	}
 }
